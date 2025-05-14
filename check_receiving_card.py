a3cf664fe63886a9beb833d1e7aa6da2eeb8efd2
Soufiane Ar Razouki <soufiane.arrazouki@clearchannel.be>
2025-05-14 12:08:58 +0200

    refactored the code to implement base monitroing

--- check_receiving_card.py
+++ check_receiving_card.py
@@ -34,7 +34,7 @@ async def main(reader, writer):
       exit_code = CRITICAL
       my_logger.info ("EXIT CODE: {}, {}".format(exit_code, message))
       end_time = time.time()
-      await icinga_output(message, exit_code, reader, writer)
+      await monitoring_log_output(message, exit_code, reader, writer)
    
    #looping through each sender card found
    i=0
@@ -52,7 +52,7 @@ async def main(reader, writer):
          message = f"Error opening serial port: {ser.name} - {str(e)}"
          exit_code = CRITICAL
          my_logger.error(message)
-         await icinga_output(message, exit_code, reader, writer)
+         await monitoring_log_output(message, exit_code, reader, writer)
          
       # -------------------------------------
       # RETRIEVE PARAMETERS FROM SENDER CARDS
@@ -73,7 +73,7 @@ async def main(reader, writer):
          except Exception as e:
             message = e
             exit_code = UNKNOWN
-            await icinga_output(message, exit_code, reader, writer)
+            await monitoring_log_output(message, exit_code, reader, writer)
       if no_of_receiver_cards != total_receiver_cards: message, exit_code = f"NO of receiver cards {no_of_receiver_cards} EXPECTED {total_receiver_cards}", CRITICAL
       else: message, exit_code = f"NO of receiver cards {no_of_receiver_cards} EXPECTED {total_receiver_cards}", GOOD
       ser.close() #closing 
@@ -91,82 +91,7 @@ async def main(reader, writer):
       # Consider including exit_code and output message into status.json     
       # ----------------------------------------------------------------
       
-      await icinga_output(message, exit_code, reader, writer)
-    
-# ------------------------------------------------------------------------------------------------------------
-# FUNCTION DEFINITIONS
-
-def search_devices(): # Searches for all sender cards connected to each USB port (/dev/ttyUSBX) on the system
-    logger = logging.getLogger(LOGGER_NAME)
-    ports = serial.tools.list_ports.comports()
-    logger.info("Found {} serial ports".format(len(ports)))
-    device_found = 0
-    valid_ports = []
-    for port, desc, hwid in sorted(ports):
-         logger.info("Searching sender card on port: " + port)
-         ser.port = port
-         try: 
-               ser.open()
-         except Exception as e:
-               logger.error(str(e))
-         if ser.isOpen():
-               logger.info("{} opened".format(port)) # remove at production
-               try:
-                  ser.flushInput() # flush input buffer, discarding all its contents
-                  ser.flushOutput() # flush output buffer, aborting current output and discard all that is in buffer
-                  ser.write (connection) # send CONNECTION command to check whether any devices are connected
-                  logger.debug("Sending command: " + ' '.join('{:02X}'.format(a) for a in connection))
-                  time.sleep (sleep_time) # allow some time for the device to respond        
-                  if ser.inWaiting()>0: # there should be something at the serial input
-                     response = ser.read(size=ser.inWaiting()) # read all the data available
-                     rx_data = list(response)
-                     logger.debug("Received data:"+' '.join('{:02X}'.format(a) for a in rx_data))
-                     if check_response(rx_data):                        
-                        if (rx_data[18]!=0 or rx_data [19]!=0): # if ACKNOWLEDGE data is not equal to zero then a device is connected
-                              # **********************************************************
-                              status[port] = {} 
-                              status[port]["lastUpdated"] = last_updated
-                              status[port]["connectedControllers"] = device_found
-                              status[port]["targetPort"] = port
-                              status[port]["controllerDescription"] = desc
-                              status[port]["controllerHardware"] = hwid
-                              # **********************************************************
-                              device_found =  device_found + 1
-                              connected_port = port
-                              valid_ports.append(port)
-                              logger.info("Device found on port: {} | {} | {}".format(port, desc, hwid))                       
-                        else:
-                              logger.info("Device not connected")
-               except Exception as e1:
-                  logger.error("Error communicating with device: " + str(e1))
-               ser.close()
-               logger.info("{} closed".format(port)) # remove at production?
-    logger.info("Found {} device(s)".format(device_found))
-    return device_found, valid_ports
-
-def check_response(received_data):
-   logger = logging.getLogger(LOGGER_NAME)
-   try:
-      if (received_data[2]==0):   
-         return True
-      else:
-         if (received_data[2]==1):
-            logger.error('Command failed due to time out (time out on trying to access devices connected to a sending card)')
-         else:
-            if (received_data[2]==2):
-               logger.error('Command failed due to check error on request data package')
-            else:
-                  if (received_data[2]==3):
-                     logger.error('Command failed due to check error on acknowledge data package')
-                  else:
-                        if (received_data[2]==4):
-                           logger.error('Command failed due to invalid command')
-                        else:
-                           logger.error('Command failed due to unkown error')
-         return False
-   except Exception as e:
-      logger.error('Command failed due to error: {}'.format(e))
-      return False
+      await monitoring_log_output(message, exit_code, reader, writer)    
 
 def get_receiver_connected(port):
 # ---------------------------------------------------------------------------------------
@@ -193,20 +118,6 @@ def get_receiver_connected(port):
       receiver_card_found = False
    return receiver_card_found
 
-
-async def icinga_output(message, exit_status, reader, writer):
-    """Outputs the result to Icinga and notifies the server."""
-    print(message)
-    try:
-        writer.write(b"Done")
-        await writer.drain()
-        await reader.read(1024)
-        writer.close()
-        await writer.wait_closed()
-        logger.info("Sent 'done' to server.")
-    except Exception as e:
-        logger.error(f"Error sending completion message: {e}")
-    sys.exit(exit_status)
 # ------------------------------------------------------------------------------------------------------------
 # PROGRAM ENTRY POINT - this won't be run only when imported from external module
 # ------------------------------------------------------------------------------------------------------------

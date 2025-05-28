e03209897ab129f22957a781c10156127e392101
Soufiane Ar Razouki <soufiane.arrazouki@clearchannel.be>
2025-05-28 10:23:06 +0200

    modified brightness

--- check_brightness.py
+++ check_brightness.py
@@ -1,264 +1,108 @@
-import serial, sys, os, time, logging, datetime, methods, subprocess, asyncio
-from serial import SerialException
-import serial.tools.list_ports
-from sys import platform
-from logging.handlers import TimedRotatingFileHandler
-from methods import read_data, write_data, loadConfig
-from command import *
-# ------------------------------------------------------------------------------------------------------------
-# DEFINITIONS AND INITIALISATIONS
-if platform == "linux":
-   dir = "/data/opt/LEDMonitoring"
-else:
-   dir = r"C:\LEDMonitoring"
-os.chdir(dir)
-
-# LOGGER
-FORMATTER = logging.Formatter('%(asctime)s %(name)s %(levelname)-8s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
-LOG_FILE = "debug_dvi.log"
-STATUS_FILE = "status.json"
-LOGGER_NAME = 'display_status'
-LOGGER_SCHEDULE = 'midnight'
-LOGGER_BACKUPS = 7
-LOGGER_INTERVAL = 1
-MAX_ATTEMPT = 5
-
-MODEL_6XX = "MSD600/MCTRL600/MCTRL610/MCTRL660"
-
-# EXIT CODES
-GOOD = 0
-WARNING = 1
-CRITICAL = 2
-UNKNOWN = 3
-
-async def communicate_with_server():
-   global data
-   global logger
-   logger = methods.get_logger(LOGGER_NAME,LOG_FILE,FORMATTER,LOGGER_SCHEDULE,LOGGER_INTERVAL,LOGGER_BACKUPS) # Set up the logging
-   """Asynchronous client communication with the server."""
-   logger.info("ESTABLISHING CONNECTION WITH LOCAL SERVER QUEUE")
-   reader, writer = await asyncio.open_connection("127.0.0.1",8888)
-   if not reader and not writer:
-      logger.error("COULD NOT ESTABLISH CONNECTION WITH 127.0.0.1 ON PORT 8888")
-   writer.write("check_brightness". encode())
-   await writer.drain()
-   logger.info("AWAITING PERMISSION TO USE COM PORTS FROM LOCAL SERVER")
-   data = await reader.read(1024)
-   if not data.decode().strip() == "START":
-      logger.error("")
-      await icinga_output("Could not make connection with localserver to access com port", UNKNOWN,reader, writer)
-   logger.info("PERMISSION TO USE COM PORT GRANTED STARTING CHECK_BRIGHTNESS SCRIPT")
-   await main(reader, writer)
-# ------------------------------------------------------------------------------------------------------------
-# MAIN
+#!/usr/bin/env python3
+import asyncio
+from base_monitoring import *
+base_script = base()
 async def main(reader, writer):
-   global sleep_time
-   global flash_wait_time
-   global status 
-   global ser
-   global last_updated
-   global data
-   global no_of_receiver_cards
-   global receiver_card_found
-   global logger
-   module_status_info = {}
-   exit_code = UNKNOWN
-   
-   
-   time.sleep(5)
-   my_logger = methods.get_logger(LOGGER_NAME,LOG_FILE,FORMATTER,LOGGER_SCHEDULE,LOGGER_INTERVAL,LOGGER_BACKUPS) # Set up the logging
-   my_logger.info("*********************************************************************************************************************************************")
-   my_logger.info("5Eyes - Starting Display Status Checks")
-   config = loadConfig(LOGGER_NAME) # Load the configuration information
-   my_logger.info("Version: {}, Baudrate: {}, Sleep Time: {}, Flash Timeout: {}".format(config["version"],config["baudrate"],config["sleepTime"],config["flashWaitTime"]))
-   last_updated = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
-   sleep_time = float(config["sleepTime"])
-   flash_wait_time = float(config["flashWaitTime"])
-   data = read_data(STATUS_FILE,LOGGER_NAME)
-   status = {} # Initialise variable to store status data\
-   modules_ok = True # assume all modules are ok to start off
-   ser = methods.setupSerialPort(config["baudrate"],LOGGER_NAME) # Initialise serial port
-   device_found, valid_ports = search_devices()
-   start_time = time.time()
-   #Validate device found on player
-   if (device_found == 0):
+   await base_script.initialize_program(reader, writer)
+   exit_code = base_script.UNKNOWN
+   monitor_message = "check_brightness"
+   output = []
+   if (base_script.device_found == 0):
       message = "NO DEVICE - make sure a valid controller is connected, that the correct baudrate is defined in config.json and ensure the NOVA LCT is not running on the host system \nThis can also mean that you don't run the tool as administrator"
-      exit_code = CRITICAL
-      my_logger.info ("EXIT CODE: {}, {}".format(exit_code, message))
-      end_time = time.time()
-      await icinga_output(message, exit_code, reader, writer)
+      exit_code = base_script.CRITICAL
+      base_script.logger.error ("EXIT CODE: {}, {}".format(exit_code, message))
+      await base_script.monitoring_log_output(message,monitor_message, exit_code, reader, writer)
    
    #looping through each sender card found
    i=0
-   for serial_port in sorted(valid_ports):
-      my_logger.info("*******************    DEVICE {}   *******************".format(i))
-      my_logger.info("Connecting to device on {}".format(serial_port))
-      ser.port = serial_port      
+   for base_script.serial_port in sorted(base_script.valid_ports):
+      i += 1
+      base_script.logger.info("*******************    DEVICE {}   *******************".format(i))
+      base_script.logger.info("Connecting to device on {}".format(base_script.serial_port))
+      base_script.logger.info(f"connected_port={base_script.serial_port}")
+      base_script.ser.port = base_script.serial_port      
       try: 
-         if ser.isOpen() == False:
-            ser.open()
-         ser.flushInput() #flush input buffer, discarding all its contents
-         ser.flushOutput() #flush output buffer, aborting current output and discard all that is in buffer
-         my_logger.info("Opened device on port: " + ser.name) # remove at production
-      except SerialException as e:
-         message = f"Error opening serial port: {ser.name} - {str(e)}"
-         exit_code = CRITICAL
-         my_logger.error(message)
-         await icinga_output(message, exit_code, reader, writer)
-         
-      # -------------------------------------
-      # RETRIEVE PARAMETERS FROM SENDER CARDS
-      # -------------------------------------
+         if base_script.ser.isOpen() == False:
+            base_script.ser.open()
+         base_script.ser.flushInput() #flush input buffer, discarding all its contents
+         base_script.ser.flushOutput() #flush output buffer, aborting current output and discard all that is in buffer
+         base_script.logger.info("Opened device on port: " + base_script.ser.name) # remove at production
+         # Retrieve parameters from sender cards
+         brightness_value, exit_code = get_display_brightness(base_script.ser.port) 
+         base_script.ser.close() #closing 
+         base_script.logger.info("Writing to JSON file")
+         base_script.logger.info("{} closed".format(base_script.ser.is_open)) # remove at production?
 
-      message, exit_code = get_display_brightness(port=ser.port) 
+         if exit_code == base_script.GOOD and brightness_value > 0: 
+            message = "Brightness OK"
+            base_script.logger.info(f"{monitor_message}=0")
+            base_script.logger.info(f"brightness_message={message}")
+            base_script.logger.info(f"brightness_value={brightness_value}%")
+            
+         else:
+            message = "Brightness Not OK" 
+            base_script.logger.critical(f"{monitor_message}=0")
+            base_script.logger.critical(f"brightness_message={message}")
+            base_script.logger.critical(f"brightness_value={brightness_value}%")
+      except Exception as e:
+         base_script.logger.error(f"{e}")
+         base_script.logger.error(f"Problem occured")      
 
-      ser.close() #closing 
-      my_logger.info("Writing to JSON file")
-      my_logger.info("{} closed".format(ser.is_open)) # remove at production?
-      # -------------------------------------------------------------
-      # TO DO
-      # Include checks for brightness >0. This should be a WARNING.
-      # -------------------------------------------------------------
+    
+   # -------------------------------------------------------------
+   # TO DO
+   # Include checks for brightness >0. This should be a WARNING.
+   # -------------------------------------------------------------
+
+   base_script.logger.info ("EXIT CODE: {}, {}".format(exit_code, message))
    
-      my_logger.info ("EXIT CODE: {}, {}".format(exit_code, message))
-      
-      # ----------------------------------------------------------------
-      # TO DO
-      # Consider including exit_code and output message into status.json     
-      # ----------------------------------------------------------------
-      print(message)
-      await icinga_output(message, exit_code, reader, writer)
+   # ----------------------------------------------------------------
+   # TO DO
+   # Consider including exit_code and output message into status.json     
+   # ----------------------------------------------------------------
+
+   await base_script.monitoring_log_output(message,monitor_message, exit_code, reader, writer)
     
 #################################################################################################
-
-def search_devices(): # Searches for all sender cards connected to each USB port (/dev/ttyUSBX) on the system
-    logger = logging.getLogger(LOGGER_NAME)
-    ports = serial.tools.list_ports.comports()
-    logger.info("Found {} serial ports".format(len(ports)))
-    device_found = 0
-    valid_ports = []
-    for port, desc, hwid in sorted(ports):
-         logger.info("Searching sender card on port: " + port)
-         ser.port = port
-         try: 
-               ser.open()
-         except Exception as e:
-               logger.error(str(e))
-         if ser.isOpen():
-               logger.info("{} opened".format(port)) # remove at production
-               try:
-                  ser.flushInput() # flush input buffer, discarding all its contents
-                  ser.flushOutput() # flush output buffer, aborting current output and discard all that is in buffer
-                  ser.write (connection) # send CONNECTION command to check whether any devices are connected
-                  logger.debug("Sending command: " + ' '.join('{:02X}'.format(a) for a in connection))
-                  time.sleep (sleep_time) # allow some time for the device to respond        
-                  if ser.inWaiting()>0: # there should be something at the serial input
-                     response = ser.read(size=ser.inWaiting()) # read all the data available
-                     rx_data = list(response)
-                     logger.debug("Received data:"+' '.join('{:02X}'.format(a) for a in rx_data))
-                     if check_response(rx_data):                        
-                        if (rx_data[18]!=0 or rx_data [19]!=0): # if ACKNOWLEDGE data is not equal to zero then a device is connected
-                              # **********************************************************
-                              status[port] = {} 
-                              status[port]["lastUpdated"] = last_updated
-                              status[port]["connectedControllers"] = device_found
-                              status[port]["targetPort"] = port
-                              status[port]["controllerDescription"] = desc
-                              status[port]["controllerHardware"] = hwid
-                              # **********************************************************
-                              device_found =  device_found + 1
-                              connected_port = port
-                              valid_ports.append(port)
-                              logger.info("Device found on port: {} | {} | {}".format(port, desc, hwid))                       
-                        else:
-                              logger.info("Device not connected")
-               except Exception as e1:
-                  logger.error("Error communicating with device: " + str(e1))
-               ser.close()
-               logger.info("{} closed".format(port)) # remove at production?
-    logger.info("Found {} device(s)".format(device_found))
-    return device_found, valid_ports
-
-def check_response(received_data):
-   logger = logging.getLogger(LOGGER_NAME)
-   try:
-      if (received_data[2]==0):   
-         return True
-      else:
-         if (received_data[2]==1):
-            logger.error('Command failed due to time out (time out on trying to access devices connected to a sending card)')
-         else:
-            if (received_data[2]==2):
-               logger.error('Command failed due to check error on request data package')
-            else:
-                  if (received_data[2]==3):
-                     logger.error('Command failed due to check error on acknowledge data package')
-                  else:
-                        if (received_data[2]==4):
-                           logger.error('Command failed due to invalid command')
-                        else:
-                           logger.error('Command failed due to unkown error')
-         return False
-   except Exception as e:
-      logger.error('Command failed due to error: {}'.format(e))
-      return False
 
 def get_display_brightness(port):
 # ---------------------------------------------------------------------------------------
 # SCREEN BRIGHTNESS SETTINGS
 # This needs to be on a per receiver card basis or global?
 # ---------------------------------------------------------------------------------------
-   logger = logging.getLogger(LOGGER_NAME)
-   logger.info("Getting current screen brightness...[TO CHECK]")
+   
+   base_script.logger.info("Getting current screen brightness...[TO CHECK]")
    display_brightness_send = methods.checksum(display_brightness)
-   logger.debug("Sending command: "+' '.join('{:02X}'.format(a) for a in display_brightness_send))
-   ser.write (display_brightness_send)
-   time.sleep (sleep_time)
-   if ser.inWaiting()>0:
-      response = ser.read(size=ser.inWaiting())
+   base_script.logger.debug("Sending command: "+' '.join('{:02X}'.format(a) for a in display_brightness_send))
+   base_script.ser.write (display_brightness_send)
+   time.sleep (base_script.sleep_time)
+   if base_script.ser.inWaiting()>0:
+      response = base_script.ser.read(size=base_script.ser.inWaiting())
       rx_data = list(response)
-      logger.debug("Received data: "+' '.join('{:02X}'.format(a) for a in rx_data))
-      if check_response(rx_data):
+      base_script.logger.debug("Received data: "+' '.join('{:02X}'.format(a) for a in rx_data))
+      if base_script.check_response(rx_data):
          brightness = rx_data[18]
          brightness_pc = round(100*brightness/255)
-         logger.info("Brightness Level: "+ str(brightness))
-         logger.info("Global Brightness: {}% ".format(brightness_pc))
-         status[port]["brightnessLevelPC"] = brightness_pc
-         status[port]["brightnessLevel"] = brightness
-         exit_code = GOOD
+         base_script.logger.info("Brightness Level: "+ str(brightness))
+         base_script.logger.info("Global Brightness: {}% ".format(brightness_pc))
+         base_script.status[port]["brightnessLevelPC"] = brightness_pc
+         base_script.status[port]["brightnessLevel"] = brightness
+         exit_code = base_script.GOOD
       else:
-         status[port]["brightnessLevelPC"] = "N/A"
-         status[port]["brightnessLevel"] = "N/A"
-         exit_code = WARNING
+         base_script.status[port]["brightnessLevelPC"] = "N/A"
+         base_script.status[port]["brightnessLevel"] = "N/A"
+         exit_code = base_script.CRITICAL
    else:
-         logger.warning("No data available at the input buffer")
-         status[port]["brightnessLevelPC"] = "N/A"
-         status[port]["brightnessLevel"] = "N/A"
-         exit_code = UNKNOWN
-   return status[port]["brightnessLevelPC"], exit_code
+         base_script.logger.warning("No data available at the input buffer")
+         base_script.status[port]["brightnessLevelPC"] = "N/A"
+         base_script.status[port]["brightnessLevel"] = "N/A"
+         exit_code = base_script.UNKNOWN
+   return base_script.status[port]["brightnessLevelPC"], exit_code
 
-async def icinga_output(message, exit_status, reader, writer):
-    """Outputs the result to Icinga and notifies the server."""
-    print(message)
-    try:
-        writer.write(b"Done")
-        await writer.drain()
-        await reader.read(1024)
-        writer.close()
-        await writer.wait_closed()
-        logger.info("Sent 'done' to server.")
-    except Exception as e:
-        logger.error(f"Error sending completion message: {e}")
-    sys.exit(exit_status)
+
 # ------------------------------------------------------------------------------------------------------------
 # PROGRAM ENTRY POINT - this won't be run only when imported from external module
 # ------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":   
-   try:      
-      asyncio.run(communicate_with_server())
-   except KeyboardInterrupt:
-      logger.info("Client shut down manually.")
-      sys.exit(UNKNOWN)
-   except Exception as e:
-      logger.exception(f"Client encountered an error: {e}")
-      sys.exit(UNKNOWN)
+   base_script._logger_name = "check_brightness"
+   asyncio.run(base_script.communicate_with_server(main))
